63f96951a626e9708341cd9afadfd51ad30bbac4
yijun20chen <yijun20chen@gmail.com>
2025-10-24 05:39:45 +0000

    Main file actually completed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from random import randint
 
-
 class Die:
     def __init__(self, sides=6):
         self.sides = sides
@@ -27,3 +26,24 @@ class DiceGame:
             return "Lose"
         else:
             return "Roll Again"
+        
+def main():
+    while True:
+        print("1. Play a round")
+        print("2. Exit")
+        try:
+            choice = int(input("Enter your choice: "))
+            if choice == 1:
+                game = DiceGame()
+                roll1, roll2, total, result = game.play_round()
+                print(f"Die 1: {roll1}, Die 2: {roll2}, Total: {total}, Result: {result}")
+            elif choice == 2:
+                break
+            else:
+                print("Invalid input. ")
+        except:
+            print("Invalid input. ")
+
+#no idea why this works but it looked cool in the other file
+if __name__ == "__main__":
+    main()
